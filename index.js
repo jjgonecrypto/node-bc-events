02ae69038441af8282a1438df97e7c2a6e96e4e5
justin j. moses <justinjmoses@gmail.com>
2018-10-06 15:30:37 -0700

    downloading token list

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 'use strict';
 
-const ethers = require('ethers');
+const https = require('https');
+const { getDefaultProvider } = require('ethers');
+const { gray, green } = require('chalk');
 
-const provider = ethers.getDefaultProvider();
+const listOfTokens = 'https://raw.githubusercontent.com/kvhnuke/etherwallet/v3.22.3/app/scripts/tokens/ethTokens.json';
 
-provider.on({address:'0x57ab1e02fee23774580c119740129eac7081e9d3'}, log => {
-  console.log(log);
+const whenTokensLoaded = new Promise((resolve, reject) => {
+  process.stdout.write(gray('Downloading list of tokens...'));
+  https.get(listOfTokens, response => {
+    let content = '';
+
+    response.on('data', d => {
+      process.stdout.write(gray('.'));
+      content += d.toString();
+    }).on('end', () => {
+      process.stdout.write(green(' Complete.\n'));
+      resolve(JSON.parse(content));
+    });
+  }).on('error', reject);
 });
+
+const tokenSymbol = process.argv[2] || 'nUSD';
+const provider = getDefaultProvider();
+
+whenTokensLoaded.then(tokens => {
+  const address = tokens.find(({ symbol }) => symbol === tokenSymbol).address;
+  console.log(gray(`Listening for ${tokenSymbol} logs (${address})`));
+  provider.on({address}, log => {
+    console.log(`\n${log}`);
+  });
+});
+
+
