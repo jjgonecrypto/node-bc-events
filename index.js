8a2451080202ac31d622dd57d89faa41edd5f6eb
justin j. moses <justinjmoses@gmail.com>
2018-10-31 20:58:36 +0100

    Cleanup

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const ora = require('ora');
 
 const { getDefaultProvider, providers: { JsonRpcProvider } } = require('ethers');
 const { gray, red, cyan } = require('chalk');
-const fetch = require('node-fetch');
 const { loadContractFromSymbol } = require('./src/contractLoader');
 
 const tokenSymbol = process.argv[2] || 'nUSD';
@@ -16,54 +15,53 @@ const whenTokenAddressLoaded = (/^0x/.test(tokenSymbol)) ? Promise.resolve(token
 
 require('dotenv-safe').config();
 
-const apikey = process.env.ETHERSCAN_API_KEY;
-
 whenTokenAddressLoaded.then(address => {
   ora({ text: gray(`Listening for ${tokenSymbol} ${tokenSymbol !== address ? `logs (${address})` : ''}`), spinner: 'rainbow' }).start();
 
-
-  provider.on('block', blockNbr => {
-    if (!blockNbr) return;
-    provider.getBlock(blockNbr).then(({ transactions }) => {
-      transactions.forEach(txn => {
-        if (!txn) return;
-        provider.getTransaction(txn).then(txn => {
-          if (!txn) return Promise.resolve([]);
-          const { to } = txn;
-          return Promise.all([Promise.resolve(to), provider.getCode(to)]);
-        }).then(([to, hex]) => {
-          return Promise.all([
-            Promise.resolve(to),
-            (hex === '0x') ?
-              Promise.resolve() :
-              fetch(`https://api.etherscan.io/api?module=contract&apikey=${apikey}&action=getsourcecode&address=${to}`).then(res => res.json()).catch(() => {})
-          ]);
-        }).then(([to, response]) => {
-          if (!response) return;
-          console.log('\nContract invocation: ', cyan(`https://etherscan.io/address/${to}`));
-          console.log(response.result[0].ContractName);
-          // console.log(response);
-        });
-      });
+  // { address } listens for all events within transactions that involve the contract
+  provider.on({ address }, log => {
+    // console.log(util.inspect(log));
+    provider.getTransaction(log.transactionHash).then(() => {
+      // console.log(util.inspect(txn));
+      return provider.getTransactionReceipt(log.transactionHash);
+    }).then(txnReceipt => {
+      const { logs, transactionHash } = txnReceipt;
+      console.log(cyan(`\nhttps://etherscan.io/tx/${transactionHash}`));
+      console.log(util.inspect(logs));
+      process.exit();
     });
   });
-
-  // { address } listens for all events within transactions that involve the contract
-  // provider.on({ address }, log => {
-  //   // console.log(util.inspect(log));
-  //   provider.getTransaction(log.transactionHash).then(txn => {
-  //     // console.log(util.inspect(txn));
-  //     return provider.getTransactionReceipt(log.transactionHash);
-  //   }).then(txnReceipt => {
-  //     const { logs, transactionHash } = txnReceipt;
-  //     console.log(cyan(`\nhttps://etherscan.io/tx/${transactionHash}`));
-  //     console.log(util.inspect(logs));
-  //     process.exit();
-  //   });
-  // });
 }).catch(err => {
   console.error(red(err));
   process.exit(1);
 });
 
+// const apikey = process.env.ETHERSCAN_API_KEY;
+// const fetch = require('node-fetch');
+//   ora({ text: gray(`Listening for contract events`, spinner: 'rainbow' }).start();
 
+// provider.on('block', blockNbr => {
+//   if (!blockNbr) return;
+//   provider.getBlock(blockNbr).then(({ transactions }) => {
+//     transactions.forEach(txn => {
+//       if (!txn) return;
+//       provider.getTransaction(txn).then(txn => {
+//         if (!txn) return Promise.resolve([]);
+//         const { to } = txn;
+//         return Promise.all([Promise.resolve(to), provider.getCode(to)]);
+//       }).then(([to, hex]) => {
+//         return Promise.all([
+//           Promise.resolve(to),
+//           (hex === '0x') ?
+//             Promise.resolve() :
+//             fetch(`https://api.etherscan.io/api?module=contract&apikey=${apikey}&action=getsourcecode&address=${to}`).then(res => res.json()).catch(() => {})
+//         ]);
+//       }).then(([to, response]) => {
+//         if (!response) return;
+//         console.log('\nContract invocation: ', cyan(`https://etherscan.io/address/${to}`));
+//         console.log(response.result[0].ContractName);
+//         // console.log(response);
+//       });
+//     });
+//   });
+// });
